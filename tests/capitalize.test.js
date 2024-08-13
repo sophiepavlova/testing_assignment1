1d1b066cf0b5443805186cace347bdac5dab9d31
sophiepavlova <olni.pavlova@gmail.com>
2024-08-13 12:13:12 -0700

    Modify tests by using assert.deepStrictEqual

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,14 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('baby') !== 'Baby') {
-  throw new Error('Функция работает неверно!');
-}
+assert.deepStrictEqual(capitalize('baby'), 'Baby');
+assert.deepStrictEqual(capitalize(''), '');
+// if (capitalize('baby') !== 'Baby') {
+//   throw new Error('Функция работает неверно!');
+// }
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
 
 console.log('Все тесты пройдены!');
